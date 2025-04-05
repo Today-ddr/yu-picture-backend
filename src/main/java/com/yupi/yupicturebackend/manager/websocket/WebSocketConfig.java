ea512e6272ebf921150e548529dcd5056241cf73
Today <xiaoqianliya@gmail.com>
2025-04-05 20:25:47 +0800

    第十二期：图片协同编辑

---
+++ src/main/java/com/yupi/yupicturebackend/manager/websocket/WebSocketConfig.java
@@ -0,0 +1,32 @@
+package com.yupi.yupicturebackend.manager.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import javax.annotation.Resource;
+
+/**
+ * WebSocket 配置（定义连接）
+ *
+ * @author Today
+ * @date 2025/04/05 03:31:02
+ */
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Resource
+    private PictureEditHandler pictureEditHandler;
+
+    @Resource
+    private WsHandshakeInterceptor wsHandshakeInterceptor;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(pictureEditHandler, "/ws/picture/edit")
+                .addInterceptors(wsHandshakeInterceptor)
+                .setAllowedOriginPatterns("*");
+    }
+}
